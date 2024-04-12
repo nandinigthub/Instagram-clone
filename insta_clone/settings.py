07515db4654b4b3b69caf34d307b191f79423842
nandini <nandinigattani9082@gmail.com>
2024-04-12 12:59:43 +0530

    .

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-7n*+usk=h7f14yhjzgfxf@ysqxjtdi=m%bg#a4j#mbc45zppsm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 # Application definition
 
@@ -139,8 +139,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = os.path.join[BASE_DIR/'static',]
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles_build','static')
+# STATIC_ROOT = 'static'
+# STATICFILES_DIRS = [BASE_DIR/'static',]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
